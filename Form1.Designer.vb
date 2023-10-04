eea08036511d5df2b7302f9bf335c9fb6db55949
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-05 00:00:12 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnMis = New Button()
+        SuspendLayout()
+        ' 
+        ' btnMis
+        ' 
+        btnMis.BackColor = SystemColors.ControlDark
+        btnMis.Location = New Point(226, 125)
+        btnMis.Name = "btnMis"
+        btnMis.Size = New Size(288, 118)
+        btnMis.TabIndex = 0
+        btnMis.Text = "Is it matching?"
+        btnMis.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.GradientActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnMis)
+        ForeColor = Color.Coral
+        Name = "Form1"
+        Text = "Data Mismatch"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnMis As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnMis_Click(sender As Object, e As EventArgs) Handles btnMis.Click
+        Dim text1, text3 As String
+        Dim text2 As Integer
+        text1 = "Visual"
+        text2 = 23
+        text3 = text1 & " " & text2
+        MsgBox(text3)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
